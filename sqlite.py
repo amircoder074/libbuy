a479fde36366413123785ec7a612cf1a1be07f48
unknown <hossseinahmadi@gmail.com>
2019-12-05 18:15:59 +0330

    add

---
+++ sqlite.py
@@ -0,0 +1,19 @@
+import sqlite3
+
+
+dbConnection = sqlite3.connect('BookLists.db')
+
+db = dbConnection.cursor()
+#db.execute("CREATE TABLE members (id integer, name text, age integer)")
+
+db.execute("SELECT * from members")
+
+#db.execute("INSERT INTO members(id, name, age) VALUES(1, 'ashkan', 14)")
+
+
+
+
+print(db.fetchall())
+dbConnection.commit()
+#print(db)
+
